fcb4f11dd361d3c49f87ee005e707f86fb672b12
Ubuntu <ubuntu@ip-10-252-104-196.us-west-2.compute.internal>
2013-07-22 04:41:26 +0000

    added own taglines

--- web.js
+++ web.js
@@ -9,6 +9,21 @@ app.get('/', function(request, response) {
   //response.send('Hello World 2!');
 });
 
+app.get('/image/:uid', function(req, res) {
+   console.log("image handler called");
+   var imgUid = req.params.uid;
+
+   var imgStream = fs.createReadStream(imgUid).on('error', function(error) {
+      var body = JSON.stringify(error);
+      res.setHeader('Content-Type', 'application/json');
+      res.setHeader('Content-Length', body.length);
+      res.end(body);
+    });
+
+    imgStream.pipe(res);
+
+});
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
